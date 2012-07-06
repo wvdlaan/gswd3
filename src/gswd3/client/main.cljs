cecc515fb4f9913305777a596a7e9abd8dcbb9b3
Walter van der Laan <walter@wvdlaan.net>
2012-07-06 12:43:44 +0200

    Added clj->js

--- src/gswd3/client/main.cljs
+++ src/gswd3/client/main.cljs
@@ -6,6 +6,22 @@
   (let [s (apply str (interpose "', '" args))]
     (.log js/console (str "log: '" s "'"))))
 
+(defn clj->js
+  "Recursively transforms ClojureScript maps into Javascript objects,
+   other ClojureScript colls into JavaScript arrays, and ClojureScript
+   keywords into JavaScript strings. This function’s approximate inverse
+   js->clj is provided by ClojureScript core.
+
+   Borrowed and updated from mmcgrana."
+  [x]
+  (cond
+   (string? x) x
+   (keyword? x) (name x)
+   (map? x) (.-strobj (reduce (fn [m [k v]]
+                                (assoc m (clj->js k) (clj->js v))) {} x))
+   (coll? x) (apply array (map clj->js x))
+   :else x))
+
 (defn ^:export service_status [jd]
   (.. d3
       (select "body")
@@ -15,6 +31,7 @@
       (enter)
       (append "li")
       (text (fn [d]
+              (log (keys (js->clj d)))
               (str (.-name d) ": " (.-status d)))))
   (.. d3
       (selectAll "li")
